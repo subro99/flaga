1d4b29e6483b09e962f824dd624d8dc627a9198d
subro <sebek.kanatek45@gmail.com>
2022-02-25 09:53:59 +0000

    zmiany generator hasla

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import os
-import random
-import string
+from haslo import generator_hasla
 
 app=Flask(__name__)
 
@@ -9,27 +8,11 @@ app=Flask(__name__)
 def haslo(): 
     return render_template("gen_haslo.html")
 
-@app.route('/wygen_haslo', methods = ["POST", "GET"])
+@app.route('/wygen_haslo', methods = ["POST"])
 def genhaslo():
-    if request.method == "GET":
-        return f"co jest kurwa"
     if request.method == "POST":
         liczba_znakow=int(request.form["liczba_znakow"])
-        # liczba_punct=2
-        # liczba_dig=2
-        # liczba_upper=2
-        # liczba_lower=2
-        znaki=[2,2,2,2]
-        while sum(znaki)<liczba_znakow:
-            i=random.randint(0,3)
-            znaki[i]+=1
-        punct=string.punctuation
-        dig=string.digits
-        uppercase=string.ascii_uppercase
-        lowercase=string.ascii_lowercase
-        haslo=random.sample(punct,znaki[0])+random.sample(dig,znaki[1])+random.sample(uppercase,znaki[2])+random.sample(lowercase,znaki[3])
-        haslo=random.sample(haslo,len(haslo))
-        haslo="".join(haslo)
+        haslo=generator_hasla(liczba_znakow)
         return render_template("wygen_haslo.html", genhaslo=haslo)
 
 @app.route('/')
