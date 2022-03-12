c4cc085dc2b9293987b18bd0815d5800dd260f4e
subro <sebek.kanatek45@gmail.com>
2022-03-12 21:55:34 +0000

    poprawka backupu

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 import os
-from haslo import generator_hasla
-
+from moje_programy.gen_data import data
+from moje_programy.haslo import generator_hasla
+from moje_programy.bohaterowie import bohater
+import random
 app=Flask(__name__)
 
 @app.route('/gen_haslo') 
@@ -12,7 +14,9 @@ def haslo():
 def genhaslo():
     if request.method == "POST":
         liczba_znakow=int(request.form["liczba_znakow"])
+        # gen_data.data(liczba_znakow)
         haslo=generator_hasla(liczba_znakow)
+        data(liczba_znakow,haslo)
         return render_template("wygen_haslo.html", genhaslo=haslo)
 
 @app.route('/')
@@ -27,6 +31,18 @@ def kubus_puchatek():
 @app.route('/xd')
 def xd():
     return render_template("xd.html")
+
+@app.route('/flaga_dla_ukrainy')
+def flaga_dla_ukrainy():
+    return render_template("flaga_ukraina.html")
+
+
+@app.route('/brudnopis')
+def brudnopis():
+    hero=bohater("ukasz")
+    heroes=["kubus","tygrysek","królik","krzyś"]
+    hero=random.choice(heroes).title()
+    return render_template("brudnopis.html", hero=hero, heroes=heroes)
 
 if __name__=="__main__":
     app.run()
