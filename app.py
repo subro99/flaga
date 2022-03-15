c5141de978d074e03e6ceff0906d4c6b463922e0
subro <sebek.kanatek45@gmail.com>
2022-03-15 10:28:14 +0000

    zajecia 15 podstrona /ciekawe-postacie

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+from audioop import reverse
 from flask import Flask, render_template, request
 import os
 from moje_programy.gen_data import data
 from moje_programy.haslo import generator_hasla
 from moje_programy.bohaterowie import bohater
 import random
+from moje_programy.postac_wiki import description_wiki, sort_description
 
-from moje_programy.postac_wiki import opis_wiki
 app=Flask(__name__)
 
 @app.route('/gen_haslo') 
@@ -39,12 +40,27 @@ def flaga_dla_ukrainy():
     return render_template("flaga_ukraina.html")
 
 
-@app.route('/brudnopis')
-def brudnopis():
+@app.route('/brudndescription')
+def brudndescription():
     hero=bohater("ukasz")
     heroes=["Kopernik","Tusk","Bruce Lee","Barack Obama"]
-    hero=opis_wiki(random.choice(heroes))
-    return render_template("brudnopis.html", hero=hero, heroes=heroes)
+    hero=description_wiki(random.choice(heroes))
+    return render_template("brudndescription.html", hero=hero, heroes=heroes)
+
+@app.route('/ciekawe-postacie')
+def int_characters():
+    c_characters=["Kopernik","Trump","Bruce Lee","Barack Obama", "Batman", "Joker"]
+    characters=random.sample(c_characters,4)
+    int_characters=[]
+    for character in characters:
+        description=description_wiki(character)
+        words=description.split()
+        counter=0
+        for word in words:
+            counter+=1
+        int_characters.append([character,description,counter])
+    int_characters.sort(reverse=True,key=sort_description)
+    return render_template("ciekawe_postacie.html",int_characters=int_characters)
 
 if __name__=="__main__":
     app.run()
--- moje_programy/postac_wiki.py
+++ moje_programy/postac_wiki.py
@@ -1,4 +1,6 @@
 import wikipedia
 wikipedia.set_lang("pl")
-def opis_wiki(postac):
+def description_wiki(postac):
     return wikipedia.summary(postac)
+def sort_description(e):
+    return e[2]
