8357b6bf978b8411049136885e117d97807287b8
subro <sebek.kanatek45@gmail.com>
2022-03-31 20:23:37 +0000

    start working on quiz answers data analysis

---
+++ moje_programy/quiz_data_analysis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+def questions_rank(data):
+    #TODO gather data about answers accuracy, and rank them from easiest to hardest
+    pass
+def hardest_question(data):
+    #TODO return hardest question with statistisc data
+    pass
+
+def easiest_question(data):
+    #TODO return easiest question with statistics data
+    pass
+
+def tricky_question(data):
+    #TODO return questions which has tricky anserws nad is commonly mismatch
+    pass
